2029ef1cd8d7798da0909570fd40aeb1bd8f1c83
mrabusalah <abdullah.adel@progressoft.com>
2020-11-04 23:11:42 +0200

    feat: adding home page content & handle app-route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {NotFound404Component} from './not-found404/not-found404.component';
-import {AppComponent} from './app.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {MainPageComponent} from './main-page/main-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: AppComponent},
+  {path: 'home', component: MainPageComponent},
+  {path: 'about-us', component: AboutUsComponent},
   {path: '**', pathMatch: 'full', redirectTo: '/404'},
   {path: '404', component: NotFound404Component}
 
